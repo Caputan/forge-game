dc47424d9d4fa9dad6136212410ee5b9136f0cf1
Artem <zmet1999@mail.ru>
2020-11-12 22:10:08 +0300

    pacman update

--- public/scripts/pacman.js
+++ public/scripts/pacman.js
@@ -2,9 +2,13 @@ let sphere;
 let box,box1,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17;
 let keysQ = [];
 var direction = new THREE.Vector3(1, 0, 0);
+let reycaster;
+var cube
+
+var mesh = []
 
 function createPacman(){
-    const pacman = new THREE.SphereGeometry(5, 0, 0);
+    const pacman = new THREE.SphereGeometry(5, 32, 32);
     const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
     const wall1 = new THREE.BoxGeometry (10,260,10);
     const wall2 = new THREE.BoxGeometry (10,10,310);
@@ -33,47 +37,32 @@ function createPacman(){
     box11 = new THREE.Mesh(wall8, wallmaterial);
     box12 = new THREE.Mesh(wall10, wallmaterial);
     box13 = new THREE.Mesh(wall12, wallmaterial);
+    
+    mesh.push(box);
+    mesh.push(box1);
+    mesh.push(box2);
+    mesh.push(box3);
+    mesh.push(box4);
+    mesh.push(box5);
+    mesh.push(box6);
+    mesh.push(box7);
+    mesh.push(box);
+
     sphere = new THREE.Mesh( pacman, material );
-     box.position.set (155,-25,145);
-     box1.position.set (155,150,-5);
-     box2.position.set (155,20,-155);
-     box3.position.set (155,-150,-5);
-     box4.position.set (155,30,100);
-     box5.position.set (155,25,75);
-     box6.position.set (155,-90,50);
-     box7.position.set (155,-60,50);
-     box8.position.set (155,-100,-50);
-     box9.position.set (155,-55,-75);
-     box10.position.set (155,-55,-100);
-     box11.position.set (155,125,-50);
-     box12.position.set (155,105,-80);
-     box13.position.set (155,50,-75);
     sphere.position.set(0, -20, 155);
     if (!NOP_VIEWER.overlays.hasScene('custom-scene')) {
         NOP_VIEWER.overlays.addScene('custom-scene');
     }
 
+    
+
     NOP_VIEWER.overlays.addMesh(sphere, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box1, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box2, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box3, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box4, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box5, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box6, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box7, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box8, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box9, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box10, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box11, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box12, 'custom-scene');
-    NOP_VIEWER.overlays.addMesh(box13, 'custom-scene');
+
     console.log(NOP_VIEWER);
 
-
+    mesh.push(cube);
 
     move();
-    
 }
 
 document.addEventListener('keydown', checkInput);
@@ -83,12 +72,12 @@ function checkInput(e){
 
         case 65:
             //sphere.position.add(new THREE.Vector3(0, 0, 1));
-            keysQ.push(new THREE.Vector3(-2, 0, 0));
+            keysQ.push(new THREE.Vector3(-1, 0, 0));
             break;
 
         case 68:
             //snake[0].position.add(new THREE.Vector3(0, 0, -1));
-            keysQ.push(new THREE.Vector3(2, 0, 0));
+            keysQ.push(new THREE.Vector3(1, 0, 0));
             break;
 
         case 87:
@@ -110,17 +99,25 @@ function move(){
         if(sphere == undefined)
             return;
 
-        direction = keysQ.length > 0 ? keysQ.pop(0) : direction;
+        let dir = new THREE.Vector3(direction.x/10, direction.y/10, direction.z/10);
+        reycaster.set(sphere.position, dir);
+    
+            
+        const intersects = reycaster.intersectObjects( mesh );
 
-        if(sphere != undefined){
-            var newPosition = new THREE.Vector3(sphere.position.x + direction.x * 0.1, 
-            sphere.position.y + direction.y * 0.1,
-            sphere.position.z + direction.z * 0.1);
+        if(intersects.length == 0){
+            direction = keysQ.length > 0 ? keysQ.pop(0) : direction;
+
+            if(sphere != undefined){
+                var newPosition = new THREE.Vector3(sphere.position.x + direction.x * 0.1, 
+                sphere.position.y + direction.y * 0.1,
+                sphere.position.z + direction.z * 0.1);
+            }
+
+            sphere.position.set(newPosition.x, newPosition.y, newPosition.z);
         }
 
-        sphere.position.set(newPosition.x, newPosition.y, newPosition.z);
-}
 
-function checkReycast(){
-
+        console.log(intersects);
+        
 }
